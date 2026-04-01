dfec98cff6cbb43945253e1bcc2e503b8bf3fa4b
JBeirne <JB1031177@wcupa.edu>
2026-04-01 11:09:51 -0400

    Fix database compilation issue

--- Rank.it/app/build.gradle.kts
+++ Rank.it/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation(libs.firebase.firestore)
     implementation(libs.core.ktx)
