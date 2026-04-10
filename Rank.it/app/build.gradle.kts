5ce152187b8eb4b0f99ecbc426e63413df8dc387
ew994895 <ew994895@wcupa.edu>
2026-04-10 12:23:34 -0400

    version updates
    for internal testing

--- Rank.it/app/build.gradle.kts
+++ Rank.it/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "edu.wcupa.csc461.rankit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
